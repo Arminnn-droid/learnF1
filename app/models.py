84c4ca3bb6240b20c521d57b5fd4b2b79c03b146
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-04 17:04:07 +0430

    models update

--- app/models.py
+++ app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30 ,help_text="it's person name", primary_key=True)
+    star = models.IntegerField("nomre", unique=True)
 
     def __str__(self):
         return self.first_name
