dc4dd6232251ab5124dee2d162007595f8adcd0f
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-07 18:02:56 +0430

    app_1 models update

--- app_1/models.py
+++ app_1/models.py
@@ -1,3 +1,22 @@
+from email.policy import default
+from tkinter import CASCADE
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('publish date')
+    
+    def __str__(self) -> str:
+        return self.text
+
+class choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    votes = models.ImageField(default=0)
+    
+    def __str__(self) -> str:
+        return self.text
