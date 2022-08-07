f6343867ad562f691dd23e9a6e5334a86bc9559c
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-07 18:10:31 +0430

    migrate

--- app_1/models.py
+++ app_1/models.py
@@ -7,16 +7,16 @@ import datetime
 # Create your models here.
 
 class Question(models.Model):
-    text = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('publish date')
+    text =       models.CharField(max_length=300)
+    pub_date =   models.DateTimeField('publish date')
     
     def __str__(self) -> str:
         return self.text
 
 class choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    votes = models.ImageField(default=0)
+    question =   models.ForeignKey(Question, on_delete=models.CASCADE)
+    text =       models.CharField(max_length=300)
+    votes =      models.ImageField(default=0)
     
     def __str__(self) -> str:
         return self.text
