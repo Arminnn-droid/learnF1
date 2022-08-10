158ba948c9cdb3e8056baec358b1e989b4db4bca
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-10 19:58:57 +0430

    app project1 models update

--- progect1/models.py
+++ progect1/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length= 300)
-    age = models.PositiveIntegerField(default=0)
-    address = models.GenericIPAddressField(null=True, blank=True)
-    salary = models.DecimalField(decimal_places= 2, max_digits=18)
+    name =     models.CharField(max_length= 300)
+    age =      models.PositiveIntegerField(default=0)
+    address =  models.GenericIPAddressField(null=True, blank=True)
+    salary =   models.DecimalField(decimal_places= 2, max_digits=18)
     
     def __str__(self) -> str:
         return self.name 
