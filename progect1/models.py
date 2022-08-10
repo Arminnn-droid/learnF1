4c527b44bb2e1a6f36976e22708d5cd1ba933e6b
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-10 19:51:20 +0430

    model Customer str added

--- progect1/models.py
+++ progect1/models.py
@@ -7,4 +7,7 @@ class Customer(models.Model):
     name = models.CharField(max_length= 300)
     age = models.PositiveIntegerField(default=0)
     address = models.GenericIPAddressField(null=True, blank=True)
-    salary = models.DecimalField(decimal_places= 2, max_digits=18) 
+    salary = models.DecimalField(decimal_places= 2, max_digits=18)
+    
+    def __str__(self) -> str:
+        return self.name 
