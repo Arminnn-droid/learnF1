f7a3f1a3dbe5c9952b417362667a36aeae759e81
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-10 19:45:43 +0430

    model customer create

--- progect1/models.py
+++ progect1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length= 300)
+    age = models.PositiveIntegerField(default=0)
+    address = models.GenericIPAddressField(null=True, blank=True)
+    salary = models.DecimalField(decimal_places= 2, max_digits=18) 
