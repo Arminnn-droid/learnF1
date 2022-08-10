7a449e815cc1c7f39e1545702c57115d146abb7f
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-10 21:28:13 +0430

    app progects2 models create

--- progect2/models.py
+++ progect2/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Clients(models.Model):
+    name =     models.CharField(max_length=300)
+    company =  models.CharField(max_length=300)
+    
+    def __str__(self) -> str:
+        return self.company
+
+class Manufacturers(models.Model):
+    name =      models.CharField(max_length=300)
+    location =  models.TextField("address")
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Products(models.Model):
+    cost_per_item =    models.PositiveBigIntegerField("Cost")
+    name_of_product =  models.CharField("name")
+    manufacturer =     models.ForeignKey(Manufacturers, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+            return self.name
+    
+class ClientOrders(models.Model):
+    fulfill_date =  models.PositiveIntegerField("Fulfill Month")
+    order_number =  models.PositiveIntegerField(primary_key=True)
+    client =        models.ForeignKey(Clients, on_delete=models.CASCADE)
+    products =      models.ManyToManyField(Products)
+    
+    def __str__(self) -> str:
+        return self.client
