b3f646657bdd6f76cc5f6e7c45209ba36c510573
Arminnn-droid <arminmalekm44@gmail.com>
2022-08-11 16:06:24 +0430

    model bank completely cereated model bank acc not complete

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,25 @@
+from turtle import mode
 from django.db import models
 
 # Create your models here.
+
+class Bank(models.Model):
+    name = models.CharField(max_length=300)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class BankAccount(models.Model):
+    bank = models.ForeignKey(on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=18, decimal_places=2)
+    account_number = models.IntegerField(max_length=18)
+    types = (
+        ('c', 'checking account'),
+        ('s', 'savings account'),
+    )
+    type_selection = models.CharField('account type', max_length=1, choices=types )
+    
+    def __str__(self) -> str:
+        return self.balance
+
+
